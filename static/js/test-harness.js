f5b4a5fabbb06b95f9b37856c25af481edbdde9c
Sam Heyman <sam.heyman@gmail.com>
2017-07-23 15:59:35 +0100

    first API call from form

--- static/js/test-harness.js
+++ static/js/test-harness.js
@@ -1,21 +1,49 @@
 (function () {
-    "use strict";
+    //"use strict";
+    var th = {};
+    var client; 
+
+    th = {
+        $loader: $("#hourglass"),
+        $submit: $("#btn-call"),
+        $reproUrl: $("#repro-url"),
+        $form: $("form"),
+
+        init: function () {
+            th.$submit.click(th._onSubmitClick);
+            $(document).on("click", ".page", function (e) {
+                e.preventDefault();
+            });
+
+        },
+
+        _onSubmitClick: function(e) {
+            e.preventDefault();
+            th.$loader.show();
+            client = new Skyscanner.LivePricingClient();
+            $formData = th.$form.serializeArray();
+            var options = {};
+            for (var i = 0; i < $formData.length; i++){
+                options[$formData[i]['name']] = $formData[i]['value'];
+            }
+            //th.$reproUrl.text(th.$form.serializeArray());
+            th.$reproUrl.show();
+            client.getHotels(options);
+        }
+
+    };
+    
+
+    th.init();
+
 
     $(document).ready(function(){
         $('[data-toggle="tooltip"]').tooltip(); 
         $('.datepicker-container .input-group.date').datepicker({
+            format: "yyyy-mm-dd",
+            startDate: "today"
         });
     });
 
-    // $(document).ready(function () {
-    //     $("#outbounddate, #checkindate").datepicker({
-    //         showOn: "focus",
-    //         dateFormat: "yy-mm-dd"
-    //     }).datepicker("setDate", "+7");
-    //     $("#inbounddate, #checkoutdate").datepicker({
-    //         showOn: "focus",
-    //         dateFormat: 'yy-mm-dd'
-    //     }).datepicker("setDate", "+14");
-    // });
 
 }());
